085ce05b05fbae985da92e0d658a293225a575b6
Ahmed Alaa <ahmed.elshoura23@gmail.com>
2020-06-21 21:14:26 +0200

    finished basic validators

---
+++ index.js
@@ -0,0 +1,11 @@
+import * as validators from './validators.js';
+
+const Validator = ()=>{
+
+	return validators
+
+}
+
+validator.isAlpha({locale: 'en-US', min: 3, max: 10, required: true});
+
+export default Validator
