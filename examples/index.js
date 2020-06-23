bcd4efea911c381934b0ebb4c930a8cccf84ff7b
Ahmed Alaa <ahmed.elshoura23@gmail.com>
2020-06-23 14:40:48 +0200

    added higher order validators. pretty much done 🎉 will just add more validators

--- examples/index.js
+++ examples/index.js
@@ -4,16 +4,38 @@ let str = "Test1235";
 
 let isValid = Validator.isAlphaNum(str);
 
-console.log(isValid);
+console.log(isValid); // true
 
 // ======================
 
-let isCombineValid = Validator.combine(Validator.isAlphaNum("test123"), Validator.isAlpha("test"));
+let isAllValid = Validator.multiple(Validator.isAlphaNum("test123"), Validator.isAlpha("test"));
 
-console.log(isCombineValid);
+console.log(isAllValid); // true
 
 // =====================
 
-let validate = Validator.validate( {valid: Validator.isAlpha, data: {param: 'test', options: {min: 3}}, name: 'firstName', required: true} );
+// // ====================== To-DO
 
-console.log(validate);
+// let isAllValid = Validator.combine("test", {valid: Validator.isAlphaNum}, {valid: Validator.isAlpha, options: {min: 3}} );
+
+// console.log(isAllValid);
+
+// // =====================
+
+
+// optional: changing the error messages
+Validator.errMsgs.isInvalid = '$ Is Wrong !';
+
+let validate = Validator.validate( {valid: Validator.isAlpha, data: {value: "Test", options: {min: 3}}, name: 'firstName', required: true} );
+console.log(validate); // true
+
+// ==========================
+
+// any RegExp
+let isValidPattern = Validator.isValidPattern("test","^[a-zA-Z]+$");
+console.log(isValidPattern); // true
+
+// ========================= high order validators
+
+let isValidUsername = Validator.isUsername("David_123");
+console.log(isValidUsername); // true
