02e0b15fb9efdec5587336084079315d789635b9
Ahmed Alaa <ahmed.elshoura23@gmail.com>
2020-06-28 03:47:23 +0200

    added isPass 👩‍💻

--- examples/index.js
+++ examples/index.js
@@ -6,22 +6,35 @@ let isValid = Validator.isAlphaNum(str);
 
 console.log(isValid); // true
 
-// ======================
+// ==========================
+
+// any RegExp
+let isValidPattern = Validator.isPattern("test","^[a-zA-Z]+$");
+console.log(isValidPattern); // true
+
+// =====================
+
+// any Text
+let isValidText = Validator.isText("Hello اهلا 😀", {min: 2});
+console.log('isValidText', isValidText); // true
+
+// ========================= high order validators
+
+let isValidUsername = Validator.isUsername("David_123");
+console.log(isValidUsername); // true
+
+//=====================
+
+let isValidPass = Validator.isPass("Pass123!#@\"'` p.s\g/ps");
+console.log("isValidPass", isValidPass); // true
+
+// ==================== Operations
 
 let isAllValid = Validator.multiple(Validator.isAlphaNum("test123"), Validator.isAlpha("test"));
 
 console.log(isAllValid); // true
 
-// =====================
-
-// // ====================== To-DO
-
-// let isAllValid = Validator.combine("test", {valid: Validator.isAlphaNum}, {valid: Validator.isAlpha, options: {min: 3}} );
-
-// console.log(isAllValid);
-
-// // =====================
-
+// ====================
 
 // optional: changing the error messages
 Validator.errMsgs.isInvalid = '$ Is Wrong !';
@@ -29,13 +42,9 @@ Validator.errMsgs.isInvalid = '$ Is Wrong !';
 let validate = Validator.validate( {valid: Validator.isAlpha, data: {value: "Test", options: {min: 3}}, name: 'firstName', required: true} );
 console.log(validate); // true
 
-// ==========================
 
-// any RegExp
-let isValidPattern = Validator.isValidPattern("test","^[a-zA-Z]+$");
-console.log(isValidPattern); // true
+// // ====================== To-DO
 
-// ========================= high order validators
+// let isAllValid = Validator.combine("test", {valid: Validator.isAlphaNum}, {valid: Validator.isAlpha, options: {min: 3}} );
 
-let isValidUsername = Validator.isUsername("David_123");
-console.log(isValidUsername); // true
+// console.log(isAllValid);
