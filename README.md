f72abdf8783d077383342f5eb380bb80a9ef1a18
Ahmed Alaa <ahmed.elshoura23@gmail.com>
2020-06-28 22:58:53 +0200

    added isEmail

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # levity-validator
-Javascript Data Validation Library
+## Javascript Data Validation Library
+
+## Installation and Usage
+
+### Server Side
+
+```
+$ npm install levity-validator
+```
+
+```
+import validator from 'levity-validator';
+
+let isValid = validator.isAlphaNum("Test1234");
+
+console.log(isValid); // true
+```
+
+### Client Side
+
+*Working on it*
+
+## Documentation
+
+- [Validators](README.md#Validators) 
+- [Operations](README.md#Operations)
+
+## Validators
+
+`
+**isAlpha(str [,options])**
+
+**only letters**
+
+options defaults: options={min: 0, max: 10000, locale: 'en-US', case: '*'} 
+locale: ['en-US','all'] - all for any letters in any language
+case: ['upper','lower','*'] - * for incase sensitive
+`
+
+`
+**isAlphaNum(str [,options])**
+
+**only letters and numbers**
+
+options defaults: options={min: 0, max: 10000, locale: 'en-US', case: '*'} 
+locale: ['en-US','all'] - all for any letters in any language
+case: ['upper','lower','*'] - * for incase sensitive
+`
--- examples/index.js
+++ examples/index.js
@@ -1,50 +1,53 @@
-import Validator from '../lib/index.js';
+import validator from '../lib/index.js';
 
-let str = "Test1235";
-
-let isValid = Validator.isAlphaNum(str);
+let isValid = validator.isAlphaNum("Test1234");
 
 console.log(isValid); // true
 
 // ==========================
 
 // any RegExp
-let isValidPattern = Validator.isPattern("test","^[a-zA-Z]+$");
+let isValidPattern = validator.isPattern("test","^[a-zA-Z]+$");
 console.log(isValidPattern); // true
 
 // =====================
 
 // any Text
-let isValidText = Validator.isText("Hello اهلا 😀", {min: 2});
+let isValidText = validator.isText("Hello اهلا 😀", {min: 2});
 console.log('isValidText', isValidText); // true
 
 // ========================= high order validators
 
-let isValidUsername = Validator.isUsername("David_123");
+let isValidUsername = validator.isUsername("David_123");
 console.log(isValidUsername); // true
 
 //=====================
 
-let isValidPass = Validator.isPass("Pass123!#@\"'` p.s\g/ps");
+let isValidPass = validator.isPass("Pass123!#@\"'` p.s\g/ps");
 console.log("isValidPass", isValidPass); // true
+
+//=====================
+
+let isValidEmail = validator.isEmail("test.test123+test@test.com");
+console.log("isValidEmail", isValidEmail); // true
 
 // ==================== Operations
 
-let isAllValid = Validator.multiple(Validator.isAlphaNum("test123"), Validator.isAlpha("test"));
+let isAllValid = validator.multiple(validator.isAlphaNum("test123"), validator.isAlpha("test"));
 
 console.log(isAllValid); // true
 
 // ====================
 
 // optional: changing the error messages
-Validator.errMsgs.isInvalid = '$ Is Wrong !';
+validator.errMsgs.isInvalid = '$ Is Wrong !';
 
-let validate = Validator.validate( {valid: Validator.isAlpha, data: {value: "Test", options: {min: 3}}, name: 'firstName', required: true} );
+let validate = validator.validate( {valid: validator.isAlpha, data: {value: "Test", options: {min: 3}}, name: 'firstName', required: true} );
 console.log(validate); // true
 
 
 // // ====================== To-DO
 
-// let isAllValid = Validator.combine("test", {valid: Validator.isAlphaNum}, {valid: Validator.isAlpha, options: {min: 3}} );
+// let isAllValid = validator.combine("test", {valid: validator.isAlphaNum}, {valid: validator.isAlpha, options: {min: 3}} );
 
 // console.log(isAllValid);
